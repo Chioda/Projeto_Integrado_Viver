351af477f47260baaf72aa228500e2ae0d14acaf
Thiago Vieira Chioda <thiagochioda@hotmail.com>
2022-05-14 14:10:50 -0300

    Deploy Heroku

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,15 @@
 
 const app = require('./src/app');
 
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Static folder
+  app.use(express.static(__dirname + '/public/'));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
